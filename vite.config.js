08f8f23ec73490407f618b896d44c5fc7e166ae7
Sunandpotru33 <sunandpotru33@gmail.com>
2025-04-10 13:53:38 +0530

    incial commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import React from 'react'
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
+})
